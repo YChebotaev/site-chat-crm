9ceffbf1bc8c12452d9e10171ada01b6f09bca35
Yury Chebotaev <yury.79120345101@gmail.com>
2019-02-23 13:34:38 +0500

    Add dynamic protocol/host to createAuth0

--- src/lib/createAuth0.js
+++ src/lib/createAuth0.js
@@ -3,7 +3,7 @@ import Auth0 from "auth0-js";
 const auth0 = new Auth0.WebAuth({
   domain: "ychebotaev.eu.auth0.com",
   clientID: "7QrzStc3GoCLFsYtdJOpM6fAs7usdfBM",
-  redirectUri: "http://localhost:3000/auth/callback",
+  redirectUri: `${window.location.protocol}//${window.location.host}/auth/callback`,
   responseType: "token id_token",
   scope: "openid"
 });
